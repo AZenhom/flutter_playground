de0b9a04c42728615eefbac17eec688612a22dd9
Ahmed Zenhom <ahmed.a.zenhom@gmail.com>
2025-09-11 11:56:38 +0400

    Implementing animation building in category_grid_view

--- lib/meal_app/presentation/category_grid_view.dart
+++ lib/meal_app/presentation/category_grid_view.dart
@@ -5,32 +5,68 @@ import 'package:flutter_playground/meal_app/presentation/meals_screen.dart';
 import 'package:flutter_playground/meal_app/providers/categories_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CategoryGridView extends ConsumerWidget {
+class CategoryGridView extends ConsumerStatefulWidget {
   const CategoryGridView({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<CategoryGridView> createState() => _CategoryGridViewState();
+}
+
+class _CategoryGridViewState extends ConsumerState<CategoryGridView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       child: ref
           .watch(categoriesProvider)
           .when(
             data: (categories) {
-              return GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 3 / 2,
-                  crossAxisSpacing: 20,
-                  mainAxisSpacing: 20,
-                ),
-                padding: const EdgeInsets.all(16),
-                itemCount: categories.length,
-                itemBuilder: (context, index) {
-                  return CategoryItem(
-                    key: ValueKey(categories[index].id),
-                    category: categories[index],
-                    onSelectCategory: (category) => navigateToCategoryMeals(category, context),
+              _animationController.forward();
+              return AnimatedBuilder(
+                animation: _animationController,
+                builder: (context, child) {
+                  return SlideTransition(
+                    child: child,
+                    position:
+                        Tween<Offset>(
+                          begin: const Offset(0, 0.7),
+                          end: const Offset(0, 0),
+                        ).animate(
+                          CurvedAnimation(
+                            parent: _animationController,
+                            curve: Curves.easeOut,
+                          ),
+                        ),
                   );
                 },
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 3 / 2,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                  ),
+                  padding: const EdgeInsets.all(16),
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    return CategoryItem(
+                      key: ValueKey(categories[index].id),
+                      category: categories[index],
+                      onSelectCategory: (category) =>
+                          navigateToCategoryMeals(category, context),
+                    );
+                  },
+                ),
               );
             },
             error: (error, stack) {
